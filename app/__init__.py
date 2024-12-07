5a19519ba673bafa3338e8049c0aac970d6518e4
pfw_Jayasri_Suresh_Vani <surej01@pfw.edu>
2024-12-07 13:12:03 -0500

    updated init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,16 @@
 from flask import Flask
-from .config import Config
+import os
 from supabase import create_client, Client
 
 def create_supabase_client():
-    supabase_url = Config.SUPABASE_URL
-    supabase_key = Config.SUPABASE_KEY
+    supabase_url = os.environ.get('SUPABASE_URL')
+    supabase_key = os.environ.get('SUPABASE_KEY')
     supabase: Client = create_client(supabase_url, supabase_key)
     return supabase
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
 
-    # Initialize Supabase client
     app.supabase_client = create_supabase_client()
 
     from .api.routes import api
